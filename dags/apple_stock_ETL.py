e2a2ad6a3e9b26225f7424fe2769076c9a6f8b86
Ouk3n <amanbanna01rajput@gmail.com>
2025-03-11 14:47:24 -0400

    updated fetch_api

--- dags/apple_stock_ETL.py
+++ dags/apple_stock_ETL.py
@@ -6,6 +6,9 @@ import json
 import requests as re
 import numpy as np
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+import sqlalchemy
+
 
 
 
@@ -26,23 +29,50 @@ with DAG (dag_id='APPL',start_date=datetime(2025,3,10),schedule_interval='@once'
         url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
         response=re.get(url)
         data=response.json()
-        file_path = '/tmp/Appl_stock_data.json'
-        with open(file_path,'w') as f:
-             json.dump(data, f)
-        return file_path
+        time_series = data.get("Time Series (Daily)", {})
+        df=pd.DataFrame(time_series).T
+        df.to_csv('appl_stocks_data')
+
 
 
     @task
     def clean_data():
-        data=pd.read_json('/tmp/Appl_stock_data.json')
-        df=pd.DataFrame(data).T
+        data=pd.read_csv('appl_stocks_data')
+        df=pd.DataFrame(data)
         df.dropna(inplace=True)
         df.drop_duplicates(inplace=True)
-        cleaned_data='/tmp/Appl_stock.csv'
+        cleaned_data='Appl_stock.csv'
         df.to_csv(cleaned_data)
-        return cleaned_data
+        
 
 
-    fetch_data()>>clean_data()
+    create_table=SQLExecuteQueryOperator(
+        task_id='creating_table',
+        conn_id='airflow_user',
+        sql="""
+        CREATE TABLE IF NOT EXISTS apple_stock(
+        date DATE PRIMARY KEY,
+        open FLOAT,
+        high FLOAT,
+        low FLOAT,
+        close FLOAT,
+        volume BIGINT );"""
+
+    )
+
+    @task
+    def upload_data(cleaned_data):
+        df=pd.read_csv(cleaned_data)
+        postgres_hook = PostgresHook(postgres_conn_id="airflow_user")
+        engine = postgres_hook.get_sqlalchemy_engine()
+
+        # Insert data into the table
+        df.to_sql('apple_stock', con=engine, if_exists='append', index=False)
+
+
+    
+
+
+    create_table() >> fetch_data() >> clean_data() >> upload_data()
 
 
