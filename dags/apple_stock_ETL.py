0b2df76e22c703871479626218f0a6c69796113c
Ouk3n <amanbanna01rajput@gmail.com>
2025-03-12 08:21:52 -0400

    fixing dag

--- dags/apple_stock_ETL.py
+++ dags/apple_stock_ETL.py
@@ -3,7 +3,7 @@ from airflow.decorators import task
 from datetime import datetime,timedelta
 import pandas as pd
 import json
-import requests as re
+import requests 
 import numpy as np
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
@@ -22,28 +22,6 @@ with DAG (dag_id='APPL',start_date=datetime(2025,3,10),schedule_interval='@once'
          ) as dag:
     
 
-    @task
-    def fetch_data():
-        api_key='U9ARU939UUHGTGGM'
-        symbol='AAPL'
-        url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
-        response=re.get(url)
-        data=response.json()
-        time_series = data.get("Time Series (Daily)", {})
-        df=pd.DataFrame(time_series).T
-        df.to_csv('appl_stocks_data')
-
-
-
-    @task
-    def clean_data():
-        data=pd.read_csv('appl_stocks_data')
-        df=pd.DataFrame(data)
-        df.dropna(inplace=True)
-        df.drop_duplicates(inplace=True)
-        cleaned_data='Appl_stock.csv'
-        df.to_csv(cleaned_data)
-        
 
 
     create_table=SQLExecuteQueryOperator(
@@ -59,10 +37,53 @@ with DAG (dag_id='APPL',start_date=datetime(2025,3,10),schedule_interval='@once'
         volume BIGINT );"""
 
     )
+    
 
     @task
-    def upload_data(cleaned_data):
-        df=pd.read_csv(cleaned_data)
+    def fetch_data():
+        api_key='U9ARU939UUHGTGGM'
+        symbol='AAPL'
+        url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+        response=requests.get(url)
+        data=response.json()
+        time_series = data.get("Time Series (Daily)", {})
+        df = pd.DataFrame.from_dict(time_series, orient='index')
+        df.reset_index(inplace=True)
+        
+        # Rename columns to match database
+        df.rename(columns={"index": "date",
+                           "1. open": "open",
+                           "2. high": "high",
+                           "3. low": "low",
+                           "4. close": "close",
+                           "5. volume": "volume"}, inplace=True)
+        file_path = "/tmp/appl_stocks_data.csv"
+        df.to_csv(file_path, index=False)
+        return file_path
+
+
+
+    @task
+    def clean_data(file_path:str):
+        df=pd.read_csv(file_path)
+        df["date"] = pd.to_datetime(df["date"]).dt.date
+        df[["open", "high", "low", "close"]] = df[["open", "high", "low", "close"]].astype(float)
+        df["volume"] = df["volume"].astype(int)
+        cleaned_data_path = "/tmp/Appl_stock_cleaned.csv"
+        df.dropna(inplace=True)
+        df.drop_duplicates(inplace=True)
+
+
+        df.to_csv(cleaned_data_path,index=False)
+        return cleaned_data_path
+        
+
+
+    
+
+    @task
+    def upload_data(cleaned_data_path):
+        df=pd.read_csv(cleaned_data_path)
         postgres_hook = PostgresHook(postgres_conn_id="airflow_user")
         engine = postgres_hook.get_sqlalchemy_engine()
 
@@ -73,6 +94,8 @@ with DAG (dag_id='APPL',start_date=datetime(2025,3,10),schedule_interval='@once'
     
 
 
-    create_table() >> fetch_data() >> clean_data() >> upload_data()
+    file_path = fetch_data()
+    cleaned_path = clean_data(file_path)
+    create_table >> file_path >> cleaned_path >> upload_data(cleaned_path)
 
 
